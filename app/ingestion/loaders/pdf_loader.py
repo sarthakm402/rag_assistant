661d718d765534805cb42c548580c3546128cb80
sarthak mohapatra <sarthakm402@gmail.com>
2026-06-23 17:25:24 +0000

    added pdf_loader and its textcleaner

---
+++ app/ingestion/loaders/pdf_loader.py
@@ -0,0 +1,22 @@
+import fitz
+from app.ingestion.utils.text_cleaner import Textcleaner
+
+class Pdfloader:
+    def load(self,pdf_path)->list[dict]:
+        doc=fitz.open(pdf_path)
+        pages=[]
+        for page_num in range(len(doc)):
+            page = doc[page_num]
+            text = page.get_text()
+            text = Textcleaner.clean(text)
+            pages.append(
+                {
+                    "page": page_num + 1,
+                    "text": text
+                }
+            )
+
+        doc.close()
+        return pages
+
+
